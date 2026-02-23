5029318450c57476a1bf18d5dd0da7a5e5cf85d8
khetpatiparn <patiparn6mgup@gmail.com>
2026-02-23 10:51:29 +0700

    handle docker compose

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PaPaiKin
+
+23/2/2569
+- create branch (connect-db)
+
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// config
+import { ConfigModule } from '@nestjs/config';
+// mongoose
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), MongooseModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
