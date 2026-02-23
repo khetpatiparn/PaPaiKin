5778acc6d6a7824446725add3f6a96a8ee6b4395
khetpatiparn <patiparn6mgup@gmail.com>
2026-02-23 17:47:59 +0700

    first commit in create-schem branch

--- backend/src/shop/schema/shop.schema.ts
+++ backend/src/shop/schema/shop.schema.ts
@@ -1,1 +1,17 @@
-export class Shop {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class Shop {
+  @Prop()
+  declare shopName: string;
+
+  @Prop()
+  declare shopImage: string;
+
+  @Prop()
+  declare location: string;
+
+  @Prop([String])
+  declare deliveryOptions: string[];
+}
