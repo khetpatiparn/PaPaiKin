d0c08f7d5573f2576a7894a9e68449ece9474c0d
khetpatiparn <patiparn6mgup@gmail.com>
2026-03-29 19:08:58 +0700

    clear dead code

--- backend/src/shop-menu-item/dto/restaurant-listing.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsMongoId, IsNotEmpty } from 'class-validator';
-
-export class RestaurantListingDto {
-  @IsMongoId()
-  @IsNotEmpty()
-  declare menuId: string;
-}
--- backend/src/shop-menu-item/dto/update-shop-menu-item.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateShopMenuItemDto } from './create-shop-menu-item.dto';
-
-export class UpdateShopMenuItemDto extends PartialType(CreateShopMenuItemDto) {}
